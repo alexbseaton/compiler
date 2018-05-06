73754b20ebba49ce054dd66365a5434aec761623
Alex Seaton <alexbseaton@gmail.com>
2018-05-06 22:33:00 +0100

    Give the tokens some meta-data

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -1,62 +1,102 @@
 import re
 
 
-def lex(path):
-    with open(path, 'r') as f:
-        program = f.read()
+class Token:
 
-    tokens = []
+
+    def __init__(self, value, n_line, n_char):
+        self.value = value
+        self.n_line = n_line
+        self.n_char = n_char
+
+
+    def __eq__(self, other):
+        return self.__class__ == other.__class__ and self.value == other.value and\
+        self.n_line == other.n_line and self.n_char == other.n_char
+
+
+    def __repr__(self):
+        return '{}[Value: [{}] n_line: [{}] n_char: [{}]]'.format(self.__class__, self.value, self.n_line, self.n_char)
+
+
+class Literal(Token):
+    pattern = r'\d+'
+    def __init__(self, value, n_line, n_char):
+        super(Literal, self).__init__(value, n_line, n_char)
+
+
+class Separator(Token):
+    pattern = r'\n'
+    def __init__(self, value, n_line, n_char):
+        super(Separator, self).__init__(value, n_line, n_char)
+
+
+class Operator(Token):
+    pattern = r'\+'
+    def __init__(self, value, n_line, n_char):
+        super(Operator, self).__init__(value, n_line, n_char)
+
+
+types = [Literal, Separator, Operator]
+
+def lex(program):
+    res = []
     current_token = None
+    n_line = 1
+    n_char = 0
     for s in program:
-        if current_token and current_token.isdigit() and s.isdigit():
-            tokens[-1] = tokens[-1] + s
+        n_char += 1
+        if current_token == Literal and re.fullmatch(Literal.pattern, s):
+            res[-1].value = res[-1].value + s
         elif s == ' ':
+            current_token = None
             continue
         else:
-            tokens.append(s)
+            candidates = [t for t in types if re.fullmatch(t.pattern, s)]
+            if not candidates:
+                raise Exception('Invalid symbol', s)
+            elif len(candidates) > 1:
+                raise Exception('Ambiguous symbol. {} matched {}'.format(s, candidates))
+            res.append(candidates[0](s, n_line, n_char))
+            current_token = candidates[0]
 
-        current_token = s
+        if s == '\n':
+            n_line += 1
 
-    return tokens
-
-
-DIGIT = 'DIGIT'
-NEWLINE = 'NEWLINE'
-PLUS = 'PLUS'
-TYPE_TO_PATTERN = {DIGIT:r'\d+', NEWLINE:r'\n', PLUS:r'\+'}
+    return res
 
 
 def accept(symbol, token):
-    return re.fullmatch(TYPE_TO_PATTERN[symbol], token)
+    return re.fullmatch(symbol.pattern, token.value)
 
 
 def statement(tokens, tree):
     current = tokens[0]
-    if accept(DIGIT, current):
-        res_tokens, res_tree = operation(tokens[1:], tree)
-        return newline(res_tokens, res_tree)
+    if accept(Literal, current):
+        res_tokens, res_tree = operator(tokens[1:], tree)
+        return separator(res_tokens, res_tree)
     else:
-        raise Exception('Parse error in statement')
+        raise Exception('Parse error in statement. Expected operator but was {}'.format(current))
 
 
-def operation(tokens, tree):
+def operator(tokens, tree):
     current = tokens[0]
-    if accept(PLUS, current):
-        return digit(tokens[1:], tree)
+    if accept(Operator, current):
+        return literal(tokens[1:], tree)
     else:
-        raise Exception('Parse error in operation')
+        raise Exception('Parse error in operation.')
 
 
-def digit(tokens, tree):
+def literal(tokens, tree):
     current = tokens[0]
-    if accept(DIGIT, current):
+    if accept(Literal, current):
         return tokens[1:], tree
     raise Exception('Parse error in digit')
 
 
-def newline(tokens, tree):
+def separator(tokens, tree):
     current = tokens[0]
-    if accept(NEWLINE, current):
+    if accept(Separator, current):
         return tokens[1:], tree
     raise Exception('Parse error in newline')
 
--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -6,10 +6,14 @@ import compiler
 class TestLex(unittest.TestCase):
 
     def test(self):
-        expected = ['53', '+', '2', '\n']
-        actual = compiler.lex('tests/program.txt')
+        program = '53 + 2\n'
+        expected = [compiler.Literal('53', 1, 1), compiler.Operator('+', 1, 4), compiler.Literal('2', 1, 6), compiler.Separator('\n', 1, 7)]
+        actual = compiler.lex(program)
         self.assertEqual(expected, actual)
 
 
 class TestParser(unittest.TestCase):
-    pass
+
+    def test(self):
+        tokens = [compiler.Literal('53', 1, 1), compiler.Operator('+', 1, 4), compiler.Literal('2', 1, 6), compiler.Separator('\n', 1, 7)]
+        compiler.statement(tokens, [])
